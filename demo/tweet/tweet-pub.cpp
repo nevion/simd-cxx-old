718844e76a49a819c52802f16018626b46c879fa
angelo.corsaro <angelo.corsaro@2a94243a-7533-11de-bf39-f1df4430aa60>
2010-06-18 13:57:10 +0000

    ChangeLogTag: 2010-06-18  Angelo Corsaro  <angelo.corsaro@gmail.com>
    git-svn-id: http://simd-cxx.googlecode.com/svn/trunk@104 2a94243a-7533-11de-bf39-f1df4430aa60

---
+++ demo/tweet/tweet-pub.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <dds/dds.hpp>
+#include "gen/ccpp_tweet.h"
+
+REGISTER_TOPIC_TRAITS(TweetType)
+
+int main(int argc, char* argv[]) {
+  
+  if (argc < 3) {
+    std::cout << "USAGE:\n\t"
+	      << "tweet-writer <uid> <partition>"
+	      << std::endl;
+    exit (-1);
+  }
+
+  std::string partition = "tweeter.";
+  partition += argv[2];
+
+  dds::Runtime runtime(partition);
+
+  dds::Duration cleanup_delay = {3600, 0};
+  
+  dds::TopicQos tQos;
+  tQos.set_persistent();
+  tQos.set_reliable();
+  tQos.set_keep_last(1024);
+  tQos.set_durability_service(cleanup_delay,
+			      DDS::KEEP_LAST_HISTORY_QOS,
+			      1024,
+			      8192,
+			      4196,
+			      8192);
+			      
+  dds::Topic<TweetType> topic("TweetTopic", tQos);
+
+  dds::DataWriterQos dwQos(tQos);
+  dwQos.set_auto_dispose(false);
+  dds::DataWriter<TweetType> dw(topic, dwQos);
+
+  std::string tweet;
+  TweetType tt;
+  tt.userId = DDS::string_dup(argv[1]);
+  std::cout << "tweet:> ";
+  while (std::getline(std::cin, tweet)) {
+    tt.tweet = DDS::string_dup(tweet.c_str());
+    dw.write(tt);
+    std::cout << "tweet:> ";
+  }
+
+  return 0;
+}
---
+++ demo/tweet/tweet-sub.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <iostream>
+#include <dds/dds.hpp>
+#include "gen/ccpp_tweet.h"
+
+REGISTER_TOPIC_TRAITS(TweetType)
+
+int main(int argc, char* argv[]) {
+  
+  if (argc < 2) {
+    std::cout << "USAGE:\n\t"
+	      << "tweet-reader <partition>"
+	      << std::endl;
+    exit (-1);
+  }
+
+  std::string partition = "tweeter.";
+  partition += argv[1];
+
+  dds::Runtime runtime(partition);
+  dds::Duration cleanup_delay = {3600, 0};
+
+  dds::TopicQos tQos;
+  tQos.set_persistent();
+  tQos.set_reliable();
+  tQos.set_keep_last(1024);
+  tQos.set_durability_service(cleanup_delay,
+			      DDS::KEEP_LAST_HISTORY_QOS,
+			      1024,
+			      8192,
+			      4196,
+			      8192);
+
+  dds::Topic<TweetType> topic("TweetTopic", tQos);
+
+
+  dds::DataReaderQos drQos(tQos);
+  dds::DataReader<TweetType> dr(topic, drQos);
+
+  TweetTypeSeq data;
+  DDS::SampleInfoSeq info;
+
+  dds::Duration timeout = {60, 0};
+  dr.wait_for_historical_data(timeout);
+
+  while (true) {
+    dr.take(data, info);
+    for (int i = 0; i < data.length(); ++i) {
+      std::cout << ">> " << data[i].userId << " :: " 
+		<< data[i].tweet << std::endl;
+    }
+    dr.return_loan(data, info);
+    sleep(1);
+  }
+  return 0;
+}
+
