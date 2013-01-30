d84d73510934dfbfe66efa2045a9cc44b7636b77
angelo.corsaro <angelo.corsaro@2a94243a-7533-11de-bf39-f1df4430aa60>
2013-01-30 11:10:37 +0000

    Added waitset to the subscriber
    git-svn-id: http://simd-cxx.googlecode.com/svn/trunk@170 2a94243a-7533-11de-bf39-f1df4430aa60

--- demo/lsa/lsa-sub.cpp
+++ demo/lsa/lsa-sub.cpp
@@ -6,6 +6,7 @@
 
 long simulationPeriod = 1000 * 1000; // usec 
 
+
 void logEntityState(const lsa::dis::EntityState& es) {
   std::cout << "Entity: "
 	    << "\n\t marking:  " << es.marking
@@ -14,16 +15,31 @@ void logEntityState(const lsa::dis::EntityState& es) {
 	    << std::endl;
 }
 
+
+class EntityStateHander {
+public:
+  void operator() (dds::DataReader<lsa::dis::EntityState>& dr) {
+    std::vector<lsa::dis::EntityState> data;
+    dr.read(data);
+    std::for_each(data.begin(), data.end(), logEntityState);
+  }
+};
+
 int main(int argc, char* argv[]) {
   dds::Runtime runtime("");
   dds::Topic<lsa::dis::EntityState> entityStateTopic("EntityStateTopic");
   dds::DataReader<lsa::dis::EntityState> dr(entityStateTopic);
 
+  EntityStateHander handler; 
+  dds::ReadCondition rc = dr.create_readcondition(handler, 
+						  dds::NOT_READ_SAMPLE_STATE, 
+						  dds::ANY_VIEW_STATE,
+						  dds::ALIVE_INSTANCE_STATE);
+  dds::WaitSet ws;
+  ws.attach(rc);
+ 
   while(true) {
-    std::vector<lsa::dis::EntityState> data;
-    dr.read(data);
-    std::for_each(data.begin(), data.end(), logEntityState);
-    usleep(simulationPeriod);
+    ws.dispatch();
   }
 
   return 0;
