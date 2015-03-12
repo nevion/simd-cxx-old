75b45ca465f35734644f08bbbc8716bd6004a5b2
Jason Newton <nevion@gmail.com>
2015-03-12 13:12:18 -0700

    make a single more standardized macro

--- cmake/OpenSplice.cmake
+++ cmake/OpenSplice.cmake
@@ -1,35 +1,36 @@
 ##############################################################################
-# OpenSplice_IDLGEN(idlfilename)
+# OpenSplice_IDLGEN(TARGET target SRC file.idl OUTPUT_PREFIX generated)
 #
-# Macro to generate OpenSplice DDS sources from a given idl file with the 
+# Macro to generate OpenSplice DDS sources from a given idl file with the
 # data structures.
-# You must include the extension .idl in the name of the data file.
 #
 ##############################################################################
 # Courtersy of Ivan Galvez Junquera <ivgalvez@gmail.com>
+# Revamped by Jason Newton <nevion@gmail.com>
 ##############################################################################
 
 
 # Macro to create a list with all the generated source files for a given .idl filename
-MACRO (DEFINE_OpenSplice_SOURCES idlfilename)
-	SET(outsources)
-	GET_FILENAME_COMPONENT(it ${idlfilename} ABSOLUTE)
-	GET_FILENAME_COMPONENT(nfile ${idlfilename} NAME_WE)
-	SET(outsources ${outsources} gen/${nfile}.cpp gen/${nfile}.h)
-	SET(outsources ${outsources} gen/${nfile}Dcps.cpp gen/${nfile}Dcps.h)
-	SET(outsources ${outsources} gen/${nfile}Dcps_impl.cpp gen/${nfile}Dcps_impl.h)
-	SET(outsources ${outsources} gen/${nfile}SplDcps.cpp gen/${nfile}SplDcps.h)
-	SET(outsources ${outsources} gen/ccpp_${nfile}.h)
-ENDMACRO(DEFINE_OpenSplice_SOURCES)
-
-MACRO (OpenSplice_IDLGEN idlfilename)
-	GET_FILENAME_COMPONENT(it ${idlfilename} ABSOLUTE)
-	GET_FILENAME_COMPONENT(idlfilename ${idlfilename} NAME)
-	DEFINE_OpenSplice_SOURCES(${ARGV})
-	ADD_CUSTOM_COMMAND (
-		OUTPUT ${outsources}
-		COMMAND ${OpenSplice_IDLGEN_BINARY}
-		ARGS  -S -l cpp  -d gen ${idlfilename}
-		DEPENDS ${it}
-	)
-ENDMACRO (OpenSplice_IDLGEN)
+include(CMakeParseArguments)
+macro(OpenSplice_IDLGEN)
+    CMAKE_PARSE_ARGUMENTS(IDLGEN "" "TARGET;SRC;OUTPUT_PREFIX" "" ${ARGN})
+    get_filename_component(IDLGEN_SRC_BASENAME ${IDLGEN_SRC} NAME_WE)
+    set(IDLGEN_OUTPUT_SRCS)
+    if(NOT IDLGEN_OUTPUT_PREFIX)
+        set(IDLGEN_OUTPUT_PREFIX "generated")
+    endif()
+    set(IDLGEN_OUTPUT_INCLUDE_PATH ${CMAKE_CURRENT_BINARY_DIR})
+    set(IDLGEN_OUTPUT_PATH ${IDLGEN_OUTPUT_INCLUDE_PATH}/${IDLGEN_OUTPUT_PREFIX})
+    list(APPEND IDLGEN_OUTPUT_SRCS ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}.cpp ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}.h)
+    list(APPEND IDLGEN_OUTPUT_SRCS ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}Dcps.cpp ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}Dcps.h)
+    list(APPEND IDLGEN_OUTPUT_SRCS ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}Dcps_impl.cpp ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}Dcps_impl.h)
+    list(APPEND IDLGEN_OUTPUT_SRCS ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}SplDcps.cpp ${IDLGEN_OUTPUT_PATH}/${IDLGEN_SRC_BASENAME}SplDcps.h)
+    list(APPEND IDLGEN_OUTPUT_SRCS ${IDLGEN_OUTPUT_PATH}/ccpp_${IDLGEN_SRC_BASENAME}.h)
+    add_custom_command(
+        OUTPUT ${IDLGEN_OUTPUT_SRCS}
+        COMMAND ${OpenSplice_IDLGEN_BINARY} ARGS -S -l cpp -d ${IDLGEN_OUTPUT_PATH} ${IDLGEN_SRC}
+        DEPENDS ${IDLGEN_SRC}
+        COMMENT "OpenSplice based IDLGEN for ${IDLGEN_SRC}"
+    )
+    add_custom_target(${IDLGEN_TARGET} DEPENDS ${IDLGEN_OUTPUT_SRCS})
+endmacro(OpenSplice_IDLGEN)
